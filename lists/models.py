0bc4501cd069206b99bb3cc4425f5b874a076cc3
Perkan <borivoje.tasovac@gmail.com>
2017-11-24 23:06:37 +0100

    Model for list Items and associated migration
    Now, database is ready to save our to-do items.

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):                   # classes that inherit from models.Model map to tables in the database (ang get auto-generated id attribute -> primary key column in the database)
+    text = models.TextField(default='')     # any other column must be defined explicitly(with default value):  this is a text field
