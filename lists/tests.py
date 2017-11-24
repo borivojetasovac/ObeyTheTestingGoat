13fee669ddff30c2d1d914ef9b21d54ab3e92344
Perkan <borivoje.tasovac@gmail.com>
2017-11-25 00:17:17 +0100

    Redirect after POST

--- lists/tests.py
+++ lists/tests.py
@@ -5,18 +5,23 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):      # HTTP GET Request requests data from a specified resource
         response = self.client.get('/')     # calls the home_page view function indirectly (via url mapping), because the given URL is root (home)
         self.assertTemplateUsed(response, 'home.html')
-    def test_can_save_a_POST_request(self):                                     # HTTP POST Request submits data to be processed to a specified resource
-        response = self.client.post('/', data={'item_text': 'A new list item'}) # to do a POST, call self.client.post: 'data' argument contains the form data we want to send
+
+    def test_can_save_a_POST_request(self):                                 # HTTP POST Request submits data to be processed to a specified resource
+        self.client.post('/', data={'item_text': 'A new list item'})        # to do a POST, call self.client.post: 'data' argument contains the form data we want to send
         
         self.assertEqual(Item.objects.count(), 1)       # objects.count() is short for objects.all().count()
         new_item = Item.objects.first()                 # the same as objects.all()[0]
         self.assertEqual(new_item.text, 'A new list item')
-        
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'}) # to do a POST, call self.client.post: 'data' argument contains the form data we want to send
+        self.assertEqual(response.status_code, 302)     # response represent an HTTP redirect (code 302) and points the browser towards a new location
+        self.assertEqual(response['location'], '/')
+    
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']   # get the POST parameter (new to-do item)
-        Item.objects.create(text=new_item_text)     # .objects.create is short for creating new Item, without needing to call .save()
-    else:
-        new_item_text = ''
+    if request.method == 'POST': 
+        Item.objects.create(text=request.POST['item_text'])     # .objects.create is short for creating and saving new Item     = ... get the POST parameter (new to-do item)
+        return redirect('/')
 
-    return render(request, 'home.html', {       # render takes the HttpRequest as its first parameter, and the name of the template to render (will search in 'templates' folder) and
-                                                #   builds an HttpResponse based on the content of the template
-        'new_item_text': new_item_text,         # pass POST parameter to template
-    })
+    return render(request, 'home.html')     # render takes the HttpRequest as its first parameter, and the name of the template to render (will search in 'templates' folder) and
+                                            #   builds an HttpResponse based on the content of the template
+
