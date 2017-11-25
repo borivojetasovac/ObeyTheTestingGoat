6157abdf5eee38a3740b4754b314d5ef38844c8e
Perkan <borivoje.tasovac@gmail.com>
2017-11-25 13:48:37 +0100

    Show all items in template
    Little bug: we need to tidy up after FT, because previous runs of the
    test are leaving stuff lying around in our database.

--- lists/views.py
+++ lists/views.py
@@ -6,6 +6,6 @@ def home_page(request):
         Item.objects.create(text=request.POST['item_text'])     # .objects.create is short for creating and saving new Item     = ... get the POST parameter (new to-do item)
         return redirect('/')
 
-    return render(request, 'home.html')     # render takes the HttpRequest as its first parameter, and the name of the template to render (will search in 'templates' folder) and
-                                            #   builds an HttpResponse based on the content of the template
-
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})   # render takes the HttpRequest as its first parameter, and the name of the template to render
+                                                            # pass 'items' to template
