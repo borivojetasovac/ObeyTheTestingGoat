7f284b5c224ac5a446e1aa04749e48e703f5d066
Perkan <borivoje.tasovac@gmail.com>
2017-11-26 14:51:51 +0100

    Add URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -4,8 +4,10 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST': 
         Item.objects.create(text=request.POST['item_text'])     # .objects.create is short for creating and saving new Item     = ... get the POST parameter (new to-do item)
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
+    return render(request, 'home.html')                         # render takes the HttpRequest as its first parameter, and the name of the template to render
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})   # render takes the HttpRequest as its first parameter, and the name of the template to render
-                                                            # pass 'items' to template
+    return render(request, 'list.html', {'items': items})
