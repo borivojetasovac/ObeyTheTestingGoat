f30214a47cb4694995726f74badde56d5d4d0f59
Perkan <borivoje.tasovac@gmail.com>
2017-11-26 20:34:48 +0100

    Adjust model to associate items with different lists

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+class List(models.Model):
+    pass
+
 class Item(models.Model):                   # classes that inherit from models.Model map to tables in the database (ang get auto-generated id attribute -> primary key column in the database)
     text = models.TextField(default='')     # any other column must be defined explicitly(with default value):  this is a text field
+    list = models.ForeignKey(List, default=None)
--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})       # render takes the HttpRequest as its first parameter, and the name of the template to render
+    return render(request, 'list.html', {'items': items})               # render takes the HttpRequest as its first parameter, and the name of the template to render
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])         # .objects.create is short for creating and saving new Item     = ... get the POST parameter (new to-do item)
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)     # .objects.create is short for creating and saving new Item     = ... get the POST parameter (new to-do item)
     return redirect('/lists/the-only-list-in-the-world/')
