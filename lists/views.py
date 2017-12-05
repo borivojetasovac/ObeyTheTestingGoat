d04040f5efcbc8cf40bbc4aa7cf9a6c2b42fc5b7
Perkan <borivoje.tasovac@gmail.com>
2017-12-05 21:24:30 +0100

    First steps of FT for layout + styling

--- lists/views.py
+++ lists/views.py
@@ -6,8 +6,8 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})               # render takes the HttpRequest as its first parameter, and the name of the template to render
-
+    return render(request, 'list.html', {'list': list_})                # render: first parameter: HttpRequest, second: the template's name,
+                                                                        # optional: a dictionary values to add to the template context
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)     # .objects.create is short for creating and saving new Item     = ... get the POST parameter (new to-do item)
