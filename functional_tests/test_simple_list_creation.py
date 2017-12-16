21049da029ab5dad48e299b1a600f0cc16f8565e
Perkan <borivoje.tasovac@gmail.com>
2017-12-16 17:28:27 +0100

    Move FTs into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,35 +1,10 @@
-import os
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import time
 
-MAX_WAIT = 10
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    def setUp(self):  
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')   # get staging server url from command line
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):  
-            self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):        # helper method: won't be run as a test, since it doesn't start with 'test_'
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
+class NewVisitorTest(FunctionalTest): 
+    
     def test_can_start_a_list_for_one_user(self):  
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -82,56 +57,32 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Now a new user, Francis, comes along to the site                  regular comments - explain the User Story
 
-        ##We use a new browser session to make sure that no information     'meta-comments' - how the test is working and why
-        ##of Edith's is coming through from cookies etc
+        ## We use a new browser session to make sure that no information     'meta-comments' - how the test is working and why
+        ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        #Francis visits the home page. There is no sign of Edith's list
+        # Francis visits the home page. There is no sign of Edith's list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
-        #Francis starts a new list by entering a new item. He is less
-        #interesting than Edith...
+        # Francis starts a new list by entering a new item. He is less
+        # interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
-        #Francis gets his own unique URL
+        # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
-        #Again, there is no trace of Edith's list
+        # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-        #Satisfied, they both go to sleep
-
-    def test_layout_and_styling(self):
-        #Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        #She notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-                inputbox.location['x'] + inputbox.size['width'] / 2,
-                512,
-                delta=10
-        )
-
-        #She starts a new list and sees the input is nicely centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-                inputbox.location['x'] + inputbox.size['width'] / 2,
-                512,
-                delta=10
-        )
+        # Satisfied, they both go to sleep
