2b6287e7bb37faad55e987d63e766ce6434836b5
Perkan <borivoje.tasovac@gmail.com>
2017-12-16 19:03:39 +0100

    Add generic explicit wait helper
    Finish off the item validation FT

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -8,7 +8,7 @@ class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_for_one_user(self):  
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)              # instead of hardcoding the vidit to localhost port 8000
+        self.browser.get(self.live_server_url)              # instead of hardcoding the visit to localhost port 8000
                                 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
