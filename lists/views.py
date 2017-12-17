367e0c439615fcec9d1953b1ff3facbea23ec0f2
Perkan <borivoje.tasovac@gmail.com>
2017-12-17 16:50:15 +0100

    Use form in all views
    Back to working state

--- lists/views.py
+++ lists/views.py
@@ -8,27 +8,20 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
+    form = ItemForm()
 
     if request.method == 'POST':
-        try:
-            item = Item(text=request.POST['text'], list=list_)
-            item.full_clean()
-            item.save()
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST['text'], list=list_)      
             return redirect(list_)                                          # redirect uses 'get_absolute_url' from List model automatically
-        except ValidationError:
-            error = "You can't have an empty list item"
+    return render(request, 'list.html', {'list': list_, "form": form})
 
-    return render(request, 'list.html', {'list': list_, 'error': error})    # render: first parameter: HttpRequest, second: the template's name,
-                                                                            # optional: a dictionary values to add to the template context
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item(text=request.POST['text'], list=list_)                 # get the POST parameter (new to-do item)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})
-    return redirect(list_)                                                  # redirect uses 'get_absolute_url' from List model automatically
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST['text'], list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form})
